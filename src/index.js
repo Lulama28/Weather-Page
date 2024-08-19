d1b99bd0f11a7a6c0ef4b63f6340e72410434ded
Karabo Manana <karabomanana28@gmail.com>
2024-08-19 22:45:20 +0200

    created my seach input in the js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+let searchInputElement = document.querySelector("#search-form-input");
+  let city = searchInputElement.value;
+  let apiKey = "210c20d975f9b48064aeo63e6381fbt6";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
 axios.get(apiUrl).then(displayTemperature);
